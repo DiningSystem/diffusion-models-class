a835dc2555e21252c3fc247c302b24cfbfa1f009
Emkay Nguyen <81498410+DiningSystem@users.noreply.github.com>
2023-03-23 16:12:21 -0500

    Update finetune_model.py

--- unit2/finetune_model.py
+++ unit2/finetune_model.py
@@ -31,6 +31,7 @@ def train(
 
     unet = UNet2DModel.from_pretrained(start_model, subfolder="unet")
     sampling_scheduler = DDIMScheduler.from_pretrained(start_model, subfolder="scheduler")
+    sampling_scheduler.set_timesteps(num_inference_steps=50)
     # Prepare pretrained model
     image_pipe = DDPMPipeline(unet, sampling_scheduler)
     #image_pipe = DDPMPipeline.from_pretrained(start_model, use_auth_token=True)
@@ -39,7 +40,7 @@ def train(
     
     # Get a scheduler for sampling
     #sampling_scheduler = DDIMScheduler.from_config(start_model, use_auth_token=True)
-    sampling_scheduler.set_timesteps(num_inference_steps=50)
+    
 
     # Prepare dataset
     dataset = load_dataset(dataset_name, split="train")
@@ -59,7 +60,7 @@ def train(
 
 
     # Optimizer & lr scheduler
-    optimizer = torch.optim.AdamW(image_pipe.unet.parameters(), lr=1e-5)
+    optimizer = torch.optim.AdamW(image_pipe.module.unet.parameters(), lr=1e-5)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
 
     for epoch in range(num_epochs):
@@ -73,14 +74,14 @@ def train(
             bs = clean_images.shape[0]
 
             # Sample a random timestep for each image
-            timesteps = torch.randint(0, image_pipe.scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
+            timesteps = torch.randint(0, image_pipe.module.scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
 
             # Add noise to the clean images according to the noise magnitude at each timestep
             # (this is the forward diffusion process)
-            noisy_images = image_pipe.scheduler.add_noise(clean_images, noise, timesteps)
+            noisy_images = image_pipe.module.scheduler.add_noise(clean_images, noise, timesteps)
 
             # Get the model prediction for the noise
-            noise_pred = image_pipe.unet(noisy_images, timesteps, return_dict=False)[0]
+            noise_pred = image_pipe.module.unet(noisy_images, timesteps, return_dict=False)[0]
 
             # Compare the prediction with the actual noise:
             loss = F.mse_loss(noise_pred, noise)
@@ -102,7 +103,7 @@ def train(
                 for i, t in tqdm(enumerate(sampling_scheduler.timesteps)):
                     model_input = sampling_scheduler.scale_model_input(x, t)
                     with torch.no_grad():
-                        noise_pred = image_pipe.unet(model_input, t)["sample"]
+                        noise_pred = image_pipe.module.unet(model_input, t)["sample"]
                     x = sampling_scheduler.step(noise_pred, t, x).prev_sample
                 grid = torchvision.utils.make_grid(x, nrow=4)
                 im = grid.permute(1, 2, 0).cpu().clip(-1, 1)*0.5 + 0.5
@@ -111,13 +112,13 @@ def train(
                 
             # Occasionally save model
             if (step+1)%save_model_every == 0:
-                image_pipe.save_pretrained(model_save_name+f'step_{step+1}')
+                image_pipe.module.save_pretrained(model_save_name+f'step_{step+1}')
 
         # Update the learning rate for the next epoch
         scheduler.step()
 
     # Save the pipeline one last time
-    image_pipe.save_pretrained(model_save_name)
-    image_pipe.push_to_hub(start_model, "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")
+    image_pipe.module.save_pretrained(model_save_name)
+    image_pipe.module.push_to_hub(start_model, "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")
     # Wrap up the run
     wandb.finish()
